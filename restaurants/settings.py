f1fb4c318343354b5516886504f49a359b2e6e4f
Amr Shedou <amrshedou@gmail.com>
2020-11-05 19:30:27 +0200

    forth commit

--- restaurants/settings.py
+++ restaurants/settings.py
@@ -131,7 +131,7 @@ MEDIA_URL = '/media/'
 PROJECT_ROOT   =   os.path.join(os.path.abspath(__file__))
 print(PROJECT_ROOT)
 print(3)
-STATIC_ROOT  =   os.path.join(PROJECT_ROOT, 'staticfiles')
+STATIC_ROOT  =   os.path.join(PROJECT_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 # Extra lookup directories for collectstatic to find static files
